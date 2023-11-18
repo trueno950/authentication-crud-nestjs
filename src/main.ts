5d40613878910ea90dc1921f20f4ae3cda33b4c4
Joel Abdiel May Canche <joel.may.mx@cloudappi.net>
2023-11-18 13:53:55 -0600

    fix: Change PORT

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.setGlobalPrefix('api/v1');
-  await app.listen(5000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
